f585e2baeedd0581c99d51769abc3f50758e093e
coque <coquelet.samuel@gmail.com>
2022-01-17 16:39:30 +0100

    fini

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
  * --> Attention à bien fermer votre fichier si vous utilisez fopen()
  */
 // TODO Votre code ici.
-
+$lorem = "Lorem ipsum dolor sit amet. Sed cumque mollitia aut blanditiis voluptatem ad possimus totam et dicta";
+$file = fopen('monFichier.txt', 'a+b');
+fwrite($file, $lorem."\n");
+fclose($file);
 
 /**
  * 3. Créez un tableau contenant au moins 10 chaînes de caractère au choix.
@@ -15,21 +18,29 @@
  * --> Attention: les chaînes de caractères doivent s'ajouter à la suite du contenu déjà existant, pas d'écrasement.
  */
 // TODO Votre code ici
+$table = ["hello", "world", "je", "m'appel", "sam"];
+$file = fopen('monFichier.txt', 'a+b');
+fwrite($file, $table."\n");
+fclose($file);
 
 
 /**
  * 5. Trouvez une solution pour afficher à l'aide d'un simple echo l'extension du fichier index.php
  */
 // TODO Votre code ici
-
+echo nl2br(file_get_contents("monFichier.txt"))."<br>";
 
 
 /**
  * 6. Testez si le fichier 'toto' existe, sil n'existe pas, afficher un texte distant que ce fichier n'existe pas !
  */
 // TODO Votre code ici.
-
-
+if (file_exists("toto.txt")) {
+    echo "Le fichier monFicher.txt existe !";
+}
+else {
+    echo "Le fichier monFicher.txt n'existe pas !";
+}
 /**
  * Super bonus.
  * Parcourrez votre fichier, à chaque fois que vous rencontrez le caractère 'a', remplacez le par le caractère '@'
